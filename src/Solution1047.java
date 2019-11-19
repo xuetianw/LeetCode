58ce11d8ecb89d03a77daa6212d5cfcac54a612d
xuetianw <xuetianw@sfu.ca>
2019-11-19 02:11:51 -0800

    all

---
+++ src/Solution1047.java
@@ -0,0 +1,19 @@
+public class Solution1047 {
+    public String removeDuplicates(String S) {
+        StringBuilder sb = new StringBuilder(S);
+        int r = sb.length() - 1;
+        int l = 0;
+        while (l <= r - 1) {
+            if (sb.charAt(l) == sb.charAt(l + 1)) {
+                sb = new StringBuilder(sb.substring(0, l) + sb.substring(l + 2, r + 1));
+                l =  (l == 0) ? 0 : l - 1;
+                r = sb.length() - 1;
+            } else l++;
+        }
+        return sb + "";
+    }
+    public static void main (String[] args) {
+        Solution1047 solution1047 = new Solution1047();
+        solution1047.removeDuplicates("abbaca");
+    }
+}
