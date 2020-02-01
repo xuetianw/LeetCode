478374edc492218c1f76d1a300bc831b5f0b316c
xuetianw <xuetianw@sfu.ca>
2020-01-31 18:37:50 -0800

    fix leepcode 709

--- src/Leetcode709.java
+++ src/Leetcode709.java
@@ -1,10 +1,22 @@
 public class Leetcode709 {
 
-    public static String toLowerCase(String str) {
-        return str.toLowerCase();
-    }
+    public String toLowerCase(String str) {
+        String res = "";
+        for (int i = 0; i < str.length(); i++) {
+            char el = str.charAt(i);
+            if (el >= 'A' && el <= 'Z') {
+                str = str.substring(0, i) + (char)(el - ('a' - 'A')) + str.substring(i + 1, str.length());
+                // res += (char)(el + ('a' - 'A'));
+            } else {
+                str += str.substring(0, i) + (char)(el - ('a' - 'A')) + str.substring(i + 1, str.length());
+            }
+        }
+        return str;
 
+    }
     public static void main(String[] args) {
+        Leetcode709 leetcode709 = new Leetcode709();
+        leetcode709.toLowerCase("Hello");
 
     }
 }
