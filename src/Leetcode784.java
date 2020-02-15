a071ff5ca9e11735f65fe3bf32560a7aa1bca8b0
xuetianw <xuetianw@sfu.ca>
2020-02-15 07:31:54 -0800

    update

--- src/Leetcode784.java
+++ src/Leetcode784.java
@@ -1,42 +1,37 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Leetcode784 {
     List<String> list = new ArrayList<>();
-    void dfs(String s, int i) {
-        if (s.length() == i) {
-            list.add(s);
+    private void dfs(char[] chars, int i) {
+        if (chars.length == i) {
+            list.add(new String(chars));
         } else {
-            char ch = s.charAt(i);
+            char ch = chars[i];
             if ('a' <= ch && ch <= 'z') {
-                ch -= 'a' - 'A';
-                dfs(s.substring(0, i) + ch + s.substring(i + 1, s.length()), i + 1);
-                dfs(s, i + 1);
+                dfs(chars, i + 1);
+                chars[i] -= 'a' - 'A';
+                dfs(chars, i + 1);
             } else if ('A' <= ch && ch <= 'Z') {
-                ch += 'A' - 'a';
-                dfs(s.substring(0, i) + ch + s.substring(i + 1, s.length()), i + 1);
-                dfs(s, i + 1);
+                dfs(chars, i + 1);
+                chars[i] += 'a' - 'A';
+                dfs(chars, i + 1);
             } else {
-                dfs(s, i + 1);
+                dfs(chars, i + 1);
             }
         }
     }
 
     public List<String> letterCasePermutation(String S) {
-        dfs(S, 0);
+        dfs(S.toCharArray(), 0);
         return list;
 
     }
 
-//    public void test(StringBuilder string) {
-//        string.append("aaaa") ;
-//    }
 
     public static void main(String[] args) {
         Leetcode784 leetcode784 = new Leetcode784();
-//        StringBuilder a = new StringBuilder("bbb");
-//        leetcode784.test(a);
-//        System.out.println(a);
-        System.out.println(leetcode784.letterCasePermutation("C").toString());
+        System.out.println(leetcode784.letterCasePermutation("a1b2").toString());
     }
 }
