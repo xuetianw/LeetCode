51807bb0988338fbb645a9d157f2fc52d610bd22
xuetianw <xuetianw@sfu.ca>
2020-03-07 23:25:04 -0800

    update

--- src/Leetcode125.java
+++ src/Leetcode125.java
@@ -2,18 +2,19 @@ import static java.lang.Character.isLetterOrDigit;
 
 public class Leetcode125 {
 
-    boolean is_char(char ch) {
-        System.out.println(ch - 'Z');
-        return ( (ch - 'A') <= ('z' - 'A')
-                && (ch -'A' >= 0));
+
+    boolean is_letterOrDigit(char ch) {
+        return (ch >= 'a' && ch <= 'z')
+                || (ch >= 'A' && ch <= 'Z')
+                || (ch >= '0' && ch <= '9');
     }
 
     public boolean isPalindrome(String s) {
         int l = 0;
         int r = s.length() - 1;
         while (l < r) {
-            while (l < r && !isLetterOrDigit(s.charAt(l))) l++;
-            while (l < r && !isLetterOrDigit(s.charAt(r))) r--;
+            while (l < r && !is_letterOrDigit(s.charAt(l))) l++;
+            while (l < r && !is_letterOrDigit(s.charAt(r))) r--;
 
             if (Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r))) {
                 return false;
--- src/Leetcode198.java
+++ src/Leetcode198.java
@@ -1,7 +1,18 @@
 public class Leetcode198 {
     public int rob(int[] nums) {
+        if (nums.length == 0) return 0;
 
-        return 0;
+        if (nums.length == 1) return nums[0];
+
+        int[] max_arr = new int[nums.length];
+        max_arr[0] = nums[0];
+        max_arr[1] = Math.max(nums[0], nums[1]);
+        // max_arr[2] = Math.max(nums[0] + nums[2], nums[1]);
+
+        for (int i = 2; i < nums.length; i++) {
+            max_arr[i] = Math.max(max_arr[i - 2] + nums[i], max_arr[i - 1]);
+        }
+        return max_arr[nums.length - 1];
     }
     public static void main(String[] args) {
         Leetcode198 leetcode198 = new Leetcode198();
