d87d3388d8463c42c53537741444b9d7ff00ad61
xuetianw <xuetianw@sfu.ca>
2020-06-04 00:07:56 -0700

    448

--- src/Array/Leetcode448.java
+++ src/Array/Leetcode448.java
@@ -8,22 +8,18 @@ import java.util.Map;
 public class Leetcode448 {
     public List<Integer> findDisappearedNumbers(int[] nums) {
         Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            map.put(i + 1, 1);
-        }
-
         for (int num : nums) {
             map.put(num, 2);
         }
 
-
         List<Integer> list = new ArrayList<>();
 
-        for (int num : map.keySet()) {
-            if (map.get(num) == 1) {
-                list.add(num);
+        for (int i = 0; i < nums.length; i++) {
+            if (map.get(i + 1) == null) {
+                list.add(i + 1);
             }
         }
+
         return list;
     }
 }
