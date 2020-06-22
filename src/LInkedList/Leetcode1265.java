829e3b796a256b0a8f31db972cb249dc7b0eb4e6
xuetianw <xuetianw@sfu.ca>
2020-06-22 01:41:03 -0700

    1265

---
+++ src/LInkedList/Leetcode1265.java
@@ -0,0 +1,19 @@
+package LInkedList;
+
+public class Leetcode1265 {
+
+      // This is the ImmutableListNode's API interface.
+      // You should not implement it, or speculate about its implementation.
+      interface ImmutableListNode {
+          public void printValue(); // print the value of this node.
+          public ImmutableListNode getNext(); // return the next node.
+      };
+
+    public void printLinkedListInReverse(ImmutableListNode head) {
+        if (head == null) {
+            return;
+        }
+        printLinkedListInReverse(head.getNext());
+        head.printValue();
+    }
+}
