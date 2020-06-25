06da3189c6dd48aeb6fc3ad57f4f7533b232b13b
xuetianw <xuetianw@sfu.ca>
2020-06-25 07:30:12 -0700

    second moethod

--- src/Sort/Leetcode973.java
+++ src/Sort/Leetcode973.java
@@ -1,20 +1,44 @@
 package Sort;
 
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 public class Leetcode973 {
+//
+//    this is the first way
+//
+
+//    public int[][] kClosest(int[][] points, int K) {
+//        Arrays.sort(points, getComparator());
+//
+//        // while (points.length > k) points.remove();
+//
+//        int[][] res = new int [K][2];
+//        for (int i = 0; i < K; i++) res[i] = points[i];
+//        return res;
+//    }
+
+
+
     public int[][] kClosest(int[][] points, int K) {
-        Arrays.sort(points, (point1, point2) ->
-             (point1[0] * point1[0] + point1[1] * point1[1]) -
-                    (point2[0] * point2[0] +  point2[1] * point2[1]));
+        PriorityQueue<int[]> pq = new PriorityQueue<>((point1, point2) ->
+                (point2[0] * point2[0] + point2[1] * point2[1]) - (point1[0] * point1[0] + point1[1] * point1[1]));
 
-        // while (points.length > k) points.remove();
+        for (int[] point : points) {
+            pq.add(point);
+            if (pq.size() > K) {
+                pq.poll();
+            }
+        }
 
-        int[][] res = new int [K][2];
-        for (int i = 0; i < K; i++) res[i] = points[i];
+        int[][] res = new int[K][2];
+        for (int i = 0; i < K; i++) {
+            res[i] = pq.poll();
+        }
+
         return res;
     }
-
 
     public void test(int[] point) {
         point = new int[] {1};
@@ -22,12 +46,7 @@ public class Leetcode973 {
 
     public static void main(String[] args) {
         Leetcode973 leetcode973 = new Leetcode973();
-//        leetcode973.kClosest(new int[][] {{1,3},{-2,2}}, 1);
-//        int[] arr = new int[]{1, 2, 3};
-//        leetcode973.test(arr);
-//
-//        System.out.println(arr);
-//        Double d = new Double("13,43d");
-////        new Boolean();
+        leetcode973.kClosest(new int[][] {{3,3}, {5, -1}, {-2,4}}, 2);
+
     }
 }
