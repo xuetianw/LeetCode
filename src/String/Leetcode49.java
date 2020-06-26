6ac08dab1ef5f964bf3c1e87669005207ff50735
xuetianw <xuetianw@sfu.ca>
2020-06-25 20:18:55 -0700

    fix 49

--- src/String/Leetcode49.java
+++ src/String/Leetcode49.java
@@ -4,46 +4,44 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Leetcode49 {
-
     boolean is_anagram(String str1, String str2) {
-        if (str1.length() != str2.length()) return false;
-        char base = 'a';
-        int[] arr = new int['z' - base];
-        for (int i = 0; i < str1.length(); i++) {
-            arr[str1.charAt(i) - base]++;
-            arr[str2.charAt(i) - base]--;
+        if (str1.length() != str2.length()) {
+            return false;
         }
 
-        for (int el : arr) {
-            if (el != 0) return false;
+        int[] base = new int['z' - 'a' + 1];
+        for (int i = 0; i < str1.length(); i++) {
+            base[str1.charAt(i) - 'a']++;
+            base[str2.charAt(i) - 'a']--;
+        }
+
+        for (int el : base) {
+            if (el != 0) {
+                return false;
+            }
         }
         return true;
     }
-    public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> ana_list = new ArrayList<>();
-        for (String str : strs) {
-            if (ana_list.size() == 0) {
-                List<String> temp = new ArrayList<>();
-                temp.add(str);
-                ana_list.add(temp);
-                continue;
-            }
 
-            boolean is_inlist = false;
-            for (List<String> list : ana_list) {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> res_list = new ArrayList<>();
+        for (String str : strs) {
+            boolean is_in_list = false;
+            for (List<String> list : res_list) {
                 if (is_anagram(list.get(0), str)) {
                     list.add(str);
-                    is_inlist = true;
+                    is_in_list = true;
+                    break;
                 }
             }
-            if (!is_inlist) {
+            if (!is_in_list) {
                 List<String> temp = new ArrayList<>();
                 temp.add(str);
-                ana_list.add(temp);
+                res_list.add(temp);
             }
 
         }
-        return ana_list;
+        return res_list;
     }
 
     public static void main(String[] args) {
