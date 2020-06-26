a86ee6f89e0a63c76c74c0f696703793556694c8
xuetianw <xuetianw@sfu.ca>
2020-06-25 21:30:29 -0700

    692 second way

--- src/Leetcode692.java
+++ src/Leetcode692.java
@@ -2,38 +2,48 @@ import java.util.*;
 
 public class Leetcode692 {
     public List<String> topKFrequent(String[] words, int k) {
-        int[] arr = new int ['z' - 'a'];
-
         Map<String, Integer> map = new HashMap<>();
-        for (String word : words) {
-            map.put(word, map.getOrDefault(word, 0) + 1);
+        for (String string : words) {
+            map.put(string, map.getOrDefault(string, 0) + 1 );
         }
         int max_count = 0;
-        for (int count : map.values()) {
-            max_count = Math.max(max_count, count);
+        for (int num : map.values()) {
+            max_count = Math.max(num, max_count);
         }
 
-        List<String> list = new ArrayList<>();
-        while (k > 0) {
+
+        List<String> res_list = new ArrayList<>();
+        int max_count_copy = max_count;
+
+
+        for (int i = max_count_copy; i >= 0; i--) {
             List<String> temp_list = new ArrayList<>();
-            for (String word : map.keySet()) {
-                if (map.get(word) == max_count) {
-                    temp_list.add(word);
-                    k--;
-                }
-                if (k == 0) {
-                    break;
+            for (String str : map.keySet()) {
+                if (map.get(str) == i) {
+                    temp_list.add(str);
                 }
             }
-            Collections.sort(temp_list, (str1, str2)-> str1.toLowerCase().compareTo(str2.toLowerCase()));
-            for (String str : temp_list) list.add(str);
-            max_count--;
+            Collections.sort(temp_list,
+                    (str1, str2) -> (str1.compareTo(str2))
+            );
+            if (res_list.size() + temp_list.size() <= k) {
+                for (String string : temp_list) {
+                    res_list.add(string);
+                }
+            } else {
+                int res_size_copy = res_list.size();
+                for (int j = 0; j < k - res_size_copy; j++) {
+                    res_list.add(temp_list.get(j));
+                }
+                break;
+            }
         }
-        return list;
+
+        return res_list;
     }
 
     public static void main(String[] args) {
         Leetcode692 leetcode692 = new Leetcode692();
-        leetcode692.topKFrequent(new String[]{"i", "love", "leetcode", "i", "love", "coding"}, 1);
+        leetcode692.topKFrequent(new String[]{"a", "aa", "aaa"}, 2);
     }
 }
