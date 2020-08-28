6f489e6d257822e5f8e51e281aecc9a1eb1f712d
xuetianw <xuetianw@sfu.ca>
2020-08-27 20:11:47 -0700

    92

--- src/LInkedList/Leetcode92.java
+++ src/LInkedList/Leetcode92.java
@@ -7,16 +7,35 @@ public class Leetcode92 {
         ListNode(int x) { val = x; }
         ListNode(int val, ListNode next) { this.val = val; this.next = next; }
     }
-//    public ListNode reverseBetween(ListNode head, int m, int n) {
-////        int location = 0;
-////        ListNode head_copy = head;
-////        ListNode dummyHead = new ListNode(1);
-////        while (head != null) {
-////            if (location == m) {
-////                dummyHead.next = head;
-////            } else if (location == n)(
-////                    dummyHead.next = head;
-////            )
-////        }
-//    }
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+        if (head == null || head.next == null || m == n) {
+            return head;
+        }
+
+        int loc = 1;
+
+        ListNode pre = null;
+        ListNode cur = head;
+        while (loc != m) {
+            pre = cur;
+            cur = cur.next;
+            loc++;
+        }
+        ListNode firstNode = pre;
+        ListNode secondNode = cur;
+
+        while (loc != n + 1) {
+            ListNode temp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = temp;
+            loc++;
+        }
+        secondNode.next = cur;
+        if (firstNode == null) { return pre; }
+        firstNode.next = pre;
+
+        return head;
+
+    }
 }
